2dda61df4efe51bc241034d75c09f5696ed49aa0
Cláudia Martins <dev.acc.4.claudia.martins@gmail.com>
2026-01-08 21:43:06 +0000

    Performance Profiler

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,9 +3,14 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Clock;
 import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
@@ -26,20 +31,33 @@ final class ProfilerImpl implements Profiler {
   }
 
   @Override
+  @SuppressWarnings("unchecked")
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
 
-    // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
-    //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
-    //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
+    List<Method> profiledMethods =
+            Arrays.stream(klass.getDeclaredMethods())
+                    .filter(m -> m.isAnnotationPresent(Profiled.class))
+                    .toList();
+    
+    if(profiledMethods.isEmpty()) {
+        throw new IllegalArgumentException("Wrapped interface does not " +
+                "contain any @Profiled methods");
+    }
 
-    return delegate;
+    return (T) Proxy.newProxyInstance(ProfilerImpl.class.getClassLoader(),
+            new Class<?>[]{klass}, new ProfilingMethodInterceptor(clock, delegate, state));
   }
 
   @Override
   public void writeData(Path path) {
-    // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
-    //       path, the new data should be appended to the existing file.
+      Objects.requireNonNull(path);
+      try(Writer writer = Files.newBufferedWriter(path)) {
+          writeData(writer);
+          writer.flush();
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
   }
 
   @Override
